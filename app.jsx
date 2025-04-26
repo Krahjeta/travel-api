b67beb19231db085ec1496a6ad39c86b33fa0fe4
YllOsmani <yo45318@ubt-uni.net>
2025-04-26 22:49:44 +0200

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './components/Home';
+import Navbar from './components/Navbar'; // nëse ke bërë Navbar më parë
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
