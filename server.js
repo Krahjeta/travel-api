6851700233b2aa59525e3b8c00dbec6c45a8c2b8
adea-1234 <adeasadikuuu123@gmail.com>
2025-05-02 22:51:26 +0200

    Add files via upload
    Shtuar backend-in dhe lidhjen me databazën

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
-const port = 3000;
+const port = 3006;
 
+// Krijo lidhje me databazën
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'destinacioni'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('U lidhëm me databazën!');
+});
+
+// Ruta bazë
 app.get('/', (req, res) => {
   res.send('Serveri po punon!');
 });
 
-app.listen(port, () => {
-  console.log(`Serveri u startua në http://localhost:${3000}`);
+// Ruta për të marrë të gjitha udhëtimet
+app.get('/udhetimet', (req, res) => {
+  db.query('SELECT * FROM udhetimet', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
+
+// Starto serverin
+app.listen(port, () => {
+  console.log(`Serveri u startua në http://localhost:${port}`);
+});
+
